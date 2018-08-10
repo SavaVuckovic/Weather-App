df1eb2b84e07044fbbf8eca82ce4b99d674e1622
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-08-10 20:26:58 +0200

    rendering info to the screen

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('#city-form');
+const weatherInfo = document.querySelector('#weather-info');
 
 const KEY = 'd320faf89d1840d0568c490f52c819d3';
 
@@ -15,14 +16,32 @@ function getWeatherByCityName(city) {
   const url = `http://api.openweathermap.org/data/2.5/weather?APPID=${KEY}&q=${city}`;
   fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => renderWeatherInfo(data));
 }
 
 // get weather by zip code
 
 
 // render weather info
-
+function renderWeatherInfo(data) {
+  weatherInfo.innerHTML = '';
+  console.log(data);
+  // city name
+  const name = document.createElement('h1');
+  name.innerText = `${data.name}, ${data.sys.country}`;
+  weatherInfo.appendChild(name);
+  // description & icon
+  const descriptionWrapper = document.createElement('div');
+  descriptionWrapper.classList.add('description');
+  const description = document.createElement('span');
+  description.innerText = data.weather[0].description;
+  const icon = document.createElement('img');
+  icon.setAttribute('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+  icon.setAttribute('alt', 'weather-icon');
+  descriptionWrapper.appendChild(description);
+  descriptionWrapper.appendChild(icon);
+  weatherInfo.appendChild(descriptionWrapper);
+}
 
 // when document is laoded
 document.addEventListener("DOMContentLoaded", () => {
