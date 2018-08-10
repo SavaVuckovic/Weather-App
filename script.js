b60acb154d826337c3dc6369d8ae792d5f551da5
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-08-10 14:22:42 +0200

    added navbar and basic form

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('test');
+const form = document.querySelector('#city-form');
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const city = e.target.elements['city'].value;
+  e.target.elements['city'].value = '';
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  form.addEventListener('submit', onFormSubmit);
+});
